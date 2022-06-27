8b1a18856490a355e12ff154c47706d8d0a95081
ashokcu <ashokcu62@gmail.com>
2022-06-27 23:18:08 +0530

    added axios in axiojs file and made it a instance , and created a constant file in  constant folder ,TMDB  server api link and api key in to two vars in that file ,then i called axios in banner js file and changed the banner title and desciption by TMDB data

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Banner.css'
+import axios from '../../axios'
+import {API_KEY}from "../../constants/constants"
+
 function Banner() {
+  const [movie,setMovie]=useState()
+useEffect(()=>{
+  axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+    console.log(response.data.results[0])
+    setMovie(response.data.results[0])
+  })
+},[])
   return (
-    <div className='banner'>
+    
+    <div className='banner'>  
       <div className='content'>
-        <h1 className='title'>movie name</h1>
+        <h1 className='title'>{movie.title}</h1>
         <div className='banner_buttons'>
           <button className='button'>play</button>
           <button className='button'>mylist</button>
         </div>
         <div>
-          <h1 className='description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi adipisci consequatur mollitia tenetur minus illo odit atque quibusdam dolorem amet repellendus </h1>
+          <h1 className='description'>{movie.overview} </h1>
         </div>
         
       </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+ export const baseUrl='https://api.themoviedb.org/3'
+ export const API_KEY="f14e4c56a60c600ab65e613a458992c1"
+ 
