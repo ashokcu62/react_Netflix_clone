15b493bf01f7c273dee69c50d3cd2a3ca40313ec
ashokcu <ashokcu62@gmail.com>
2022-07-02 21:00:39 +0530

    i used TMDB -the tmdb  api , fetched in to banner and rowPost

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,27 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import './Banner.css'
 import axios from '../../axios'
-import {API_KEY}from "../../constants/constants"
+import {API_KEY,imageUrl}from "../../constants/constants"
 
 function Banner() {
   const [movie,setMovie]=useState()
+  let x = Math.floor((Math.random() * 20) + 1);
 useEffect(()=>{
   axios.get(`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
-    console.log(response.data.results[0])
-    setMovie(response.data.results[0])
+    console.log(response.data.results[x])
+    console.log(x)
+    setMovie(response.data.results[x])
   })
 },[])
   return (
     
-    <div className='banner'>  
+    <div className='banner'
+    style={{ backgroundImage:`url(${movie ? imageUrl+movie.backdrop_path :""})` }}
+    >  
+   
       <div className='content'>
-        <h1 className='title'>{movie.title}</h1>
+        <h1 className='title'>{movie ? movie.name:"movie"}</h1>
         <div className='banner_buttons'>
           <button className='button'>play</button>
           <button className='button'>mylist</button>
         </div>
         <div>
-          <h1 className='description'>{movie.overview} </h1>
+          <h1 className='description'>{movie ? movie.overview :" overview"} </h1>
         </div>
         
       </div>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,5 @@
  export const baseUrl='https://api.themoviedb.org/3'
  export const API_KEY="f14e4c56a60c600ab65e613a458992c1"
+ export const imageUrl = 'https://image.tmdb.org/t/p/original'
+
  
